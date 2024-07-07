8f1084fde8e45d559c77049bb689257ba8cbd641
aidamouref <aidamouref@gmail.com>
2024-07-07 14:45:22 +0200

    add new requirements in the requirements.txt

--- pages/1  Traveler Reviews.py
+++ pages/1  Traveler Reviews.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
 import nltk
 
 # Load the sentiment data
--- pages/4  Airline Recommendator.py
+++ pages/4  Airline Recommendator.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
 import streamlit.components.v1 as components
 import codecs
 from sklearn.neighbors import NearestNeighbors
